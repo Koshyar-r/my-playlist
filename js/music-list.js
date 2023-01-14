bb5b1169eaedb58035045394a33178f0e757df50
Koshyar-r <belfi7777@gmail.com>
2023-01-14 01:52:07 -0800

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,194 @@
+let AllMusic = [
+    {
+      name: "Wake Me Up",
+      artist: "Avici",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Calm Down",
+      artist: "Rema, Selena Gomez",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "NDA",
+      artist: "Billie Eilish",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Unholy",
+      artist: "Sam Smith Ft. Kim Petras",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "In Rahesh Nist",
+      artist: "Shayan Eshraghi",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "oon hata ba txtash ba harfash",
+      artist: "Amir Tataloo",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Sange Ghabre Arezoo",
+      artist: "Artoosh",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Magic",
+      artist: "Coldplay",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Don't stop the party",
+      artist: "Pitbull (Ft TJR)",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "Fireball (Ft John Ryan)",
+      artist: "Pitbull",
+      img: "music-10",
+      src: "music-10"
+    },
+    {
+      name: "Slow Down",
+      artist: "Selena Gomez",
+      img: "music-11",
+      src: "music-11"
+    },
+    {
+      name: "Stereo Hearts",
+      artist: "Adam Levine",
+      img: "music-12",
+      src: "music-12"
+    },
+    {
+      name: "Big Boys",
+      artist: "Sza",
+      img: "music-13",
+      src: "music-13"
+    },
+    {
+      name: "Harchiam Beshe",
+      artist: "Wantons (Ft Nassim & AFX)",
+      img: "music-14",
+      src: "music-14"
+    },
+    {
+      name: "Pas Man Chi",
+      artist: "Wantons",
+      img: "music-15",
+      src: "music-15"
+    },
+    {
+      name: "Uncover",
+      artist: "Zara Larsson",
+      img: "music-16",
+      src: "music-16"
+    },
+    {
+      name: "To Boodi",
+      artist: "Zhak",
+      img: "music-17",
+      src: "music-17"
+    },
+    {
+      name: "Sanieh Var",
+      artist: "Aron Afshar",
+      img: "music-18",
+      src: "music-18"
+    },
+    {
+      name: "Kojaei Yani",
+      artist: "Mr Kiarash",
+      img: "music-19",
+      src: "music-19"
+    },
+    {
+      name: "Mood",
+      artist: "Makar",
+      img: "music-20",
+      src: "music-20"
+    },
+    {
+      name: "PSY 2014",
+      artist: "Dj Kia",
+      img: "music-21",
+      src: "music-21"
+    },
+    {
+      name: "Se Te Nota",
+      artist: "Lele Pons & Guaynaa",
+      img: "music-22",
+      src: "music-22"
+    },
+    {
+      name: "Mocking Bird",
+      artist: "Eminem",
+      img: "music-23",
+      src: "music-23"
+    },
+    {
+      name: "Control",
+      artist: "Zoe Wees",
+      img: "music-24",
+      src: "music-24"
+    },
+    {
+      name: "Henna Remix",
+      artist: "Cameron Cartio (Ft Khaled)",
+      img: "music-25",
+      src: "music-25"
+    },
+    {
+      name: "I can't help falling in love with u",
+      artist: "Elvis Presley",
+      img: "music-26",
+      src: "music-26"
+    },
+    {
+      name: "Extasy",
+      artist: "Koorosh (Ft Sijal & Sami Low & Raha)",
+      img: "music-27",
+      src: "music-27"
+    },
+    {
+      name: "Magic",
+      artist: "Icona Pop (Ft Charli XCX)",
+      img: "music-28",
+      src: "music-28"
+    },
+    {
+      name: "Good time",
+      artist: "Inna (Ft Pitbull)",
+      img: "music-29",
+      src: "music-29"
+    },
+    {
+      name: "maybe my soulmate died",
+      artist: "iamnotshane",
+      img: "music-30",
+      src: "music-30"
+    },
+    {
+      name: "baram mian kia bad to",
+      artist: "Unknown",
+      img: "music-31",
+      src: "music-31"
+    },
+    {
+      name: "Look at her now",
+      artist: "Selena Gomez",
+      img: "music-32",
+      src: "music-32"
+    },
+  ]
